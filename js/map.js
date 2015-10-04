99d824e7ff03558255231a5e1ea651a04417447a
Jouella <jojofabe@vt.edu>
2015-10-04 04:13:16 -0400

    added more files

--- js/map.js
+++ js/map.js
@@ -1,5 +1,19 @@
 var map = new Datamap({
-    scope: 'usa',
     element: document.getElementById('map'),
+    geographyConfig: {
+        dataUrl: 'nyctopo.json'
+    },
+    scope: 'custom',
+    setProjection: function(element, options) {
+        var projection, path;
+        projection = d3.geo.albersUsa()
+            .center([long, lat])
+            .scale(element.offsetWidth)
+            .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
+        path = d3.geo.path()
+            .projection( projection );
+        return {path: path, projection: projection};
+    }
 });
+
